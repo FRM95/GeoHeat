47421da70c613130202dc832f4b292186d6757cb
Miguel Moreno <miguelmm1995@gmail.com>
2024-09-10 20:39:57 +0200

    exit! created docker container for mongodb and authentication

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -1,11 +1,7 @@
-set -e
-mongosh <<EOF
-use admin
-db.createUser({
-  user: 'root',
-  pwd:  '1h4v3cr34t3d4r00t9s3r',
-  roles: [{
-    role: 'userAdminAnyDatabase'
-  }]
-})
-EOF
+# Seeding mongodb with default data
+mongoimport --authenticationDatabase=$MONGO_INITDB_DATABASE -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD -d=$REQUESTDB -c=$REQUESTDB_areas --file=$REQUESTDB_areas_file --drop --jsonArray && \
+mongoimport --authenticationDatabase=$MONGO_INITDB_DATABASE -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD -d=$REQUESTDB -c=$REQUESTDB_countries --file=$REQUESTDB_countries_file --drop --jsonArray && \
+mongoimport --authenticationDatabase=$MONGO_INITDB_DATABASE -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD -d=$REQUESTDB -c=$REQUESTDB_firms --file=$REQUESTDB_firms_file --drop --jsonArray && \
+mongoimport --authenticationDatabase=$MONGO_INITDB_DATABASE -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD -d=$GEOSPATIALDB -c=$GEOSPATIALDB_world_data --file=$GEOSPATIALDB_world_data_file --drop && \
+mongoimport --authenticationDatabase=$MONGO_INITDB_DATABASE -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD -d=$THREEJSDB -c=$THREEJSDB_lights --file=$THREEJSDB_lights_file --drop --jsonArray && \
+mongoimport --authenticationDatabase=$MONGO_INITDB_DATABASE -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD -d=$THREEJSDB -c=$THREEJSDB_textures --file=$THREEJSDB_textures_file --drop --jsonArray
